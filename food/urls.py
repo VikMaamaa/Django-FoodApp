4dd2b22d00b73b9fdeafbc529bc0636c14df1afd
vikmaamaa <maamaavictor@yahoo.com>
2023-02-20 02:16:05 +0100

    feat: created form to add items

--- food/urls.py
+++ food/urls.py
@@ -7,4 +7,6 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('item/',views.item, name='item'),
     path('<int:item_id>/', views.detail, name='detail')
+    # add items
+    path('add', views.create_item, name='create_item'),
 ]
