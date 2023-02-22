c3a0bf4fa226d5c4b804c4f900f60578ca60de99
vikmaamaa <maamaavictor@yahoo.com>
2023-02-22 15:53:29 +0100

    feat: adding class based view

--- food/urls.py
+++ food/urls.py
@@ -4,7 +4,7 @@ from django.urls import path
 app_name = 'food'
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', views.IndexClassView.as_view(), name='index'),
     path('item/',views.item, name='item'),
     path('<int:item_id>/', views.detail, name='detail'),
     # add items
